4582ab59476080d734dda0da7a0a2b95691b1202
Janos Hausberger <hausberger.janos@codeyard.eu>
2019-11-13 11:11:34 +0100

    okHttpClient has been added

--- app/src/main/java/com/codingwithmitch/mviexample/api/MyRetrofitBuilder.kt
+++ app/src/main/java/com/codingwithmitch/mviexample/api/MyRetrofitBuilder.kt
@@ -1,18 +1,35 @@
 package com.codingwithmitch.mviexample.api
 
+import com.codingwithmitch.mviexample.BuildConfig
 import com.codingwithmitch.mviexample.util.LiveDataCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object MyRetrofitBuilder {
 
     const val BASE_URL: String = "https://open-api.xyz/"
 
+    val okHttpClientBuilder: OkHttpClient.Builder by lazy {
+        OkHttpClient.Builder()
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+    }
+
     val retrofitBuilder: Retrofit.Builder by lazy {
+        if (BuildConfig.DEBUG) {
+            val logging = HttpLoggingInterceptor()
+            logging.level = HttpLoggingInterceptor.Level.BODY
+            okHttpClientBuilder.addInterceptor(logging)
+        }
+
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addCallAdapterFactory(LiveDataCallAdapterFactory())
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClientBuilder.build())
     }
 
 
